5f0575beaae5a6de8e789d0e0a2bb8b3524e8777
Neba Schneider Kintalen <desneirdero@gmail.com>
2020-07-22 10:17:42 +0100

    added feature staging branch

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Chaii man don suffer
+          Deployed from staging branch
         </p>
         <a
           className="App-link"
