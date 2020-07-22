d5afb0419d3a2c1a1320ad2b43d83ec2cdc3000a
Neba Schneider Kintalen <desneirdero@gmail.com>
2020-07-22 09:27:48 +0100

    Added espose 80

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Hot reload in containers must work
+          Chaii man don suffer
         </p>
         <a
           className="App-link"
